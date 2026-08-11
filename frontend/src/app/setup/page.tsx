d70310a55d8ad96dcd0abded817b43cb4d7c0586
seroxdesign <sherifcherfa@gmail.com>
2026-08-11 13:54:23 -0400

    refactor(setup): let the wizard own its state

--- frontend/src/app/setup/page.tsx
+++ frontend/src/app/setup/page.tsx
@@ -1,9 +1,7 @@
 "use client";
 
 import { SetupView } from "@/features/setup/setup-view/setup-view";
-import { useSetup } from "@/features/setup/use-setup";
 
 export default function SetupPage() {
-  const setup = useSetup();
-  return <SetupView {...setup} />;
+  return <SetupView />;
 }
